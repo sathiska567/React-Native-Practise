826710179f629ee1b499bb79e8dcd3d4e78e6d7a
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-05-29 20:27:43 +0530

    updated

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import LoginPage from './pages/LoginPage';
 import SplashScreenPage from './pages/splashScreenPage';
@@ -6,11 +6,61 @@ import SignUp from './pages/SignUp';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomePage from './pages/UsersPage/HomePage';
-import DashBoad from './pages/Admin/DashBoad';
-import FlatScreen from './screen/FlatList';
+import Dashboard from './pages/Admin/DashBoad';
 import UserDetails from './pages/Admin/UserDetails';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Profile from './pages/UsersPage/Profile';
+import AddCreate from './pages/UsersPage/AddCreate';
+import BrowsAdd from './pages/UsersPage/BrowsAdd';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import image from "./assets/images/travel.png"
+
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainTabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home Page') {
+          iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'Profile') {
+          iconName = focused ? 'person' : 'person-outline';
+        } else if (route.name === 'Add_Create') {
+          iconName = focused ? 'add-circle' : 'add-circle-outline';
+        } else if (route.name === 'Brows_Add') {
+          iconName = focused ? 'list' : 'list-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+      
+    })}
+  >
+    <Tab.Screen name="Home Page" component={HomePage} options={{
+       headerTitle: () => (
+        <Image
+          source={image}
+          style={{ width: '100vw', height: '10vh' }}
+        />
+      ),
+      headerStyle: styles.homePageHeader,
+      headerTitleStyle:styles.HomeHeaderTitle
+
+      }}/>
+
+    <Tab.Screen name="Add_Create" component={AddCreate} />
+    <Tab.Screen name="Brows_Add" component={BrowsAdd} />
+    <Tab.Screen name="Profile" component={Profile} />
+
+  </Tab.Navigator>
+);
 
 const App = () => {
   const [isShowSplash, setIsShowSplash] = useState(true);
@@ -48,14 +98,13 @@ const App = () => {
               shadowOpacity: 0.2, // For iOS
               shadowRadius: 2, // For iOS
               elevation: 5, // For Android
-            }
+            },
           }}
-
         />
         <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Home Page" component={HomePage} />
-        <Stack.Screen name="dashboad" component={DashBoad} />
+        <Stack.Screen name="Home Page" component={MainTabNavigator} options={{ headerShown: false}} />
         <Stack.Screen name="userDetails" component={UserDetails} />
+        <Stack.Screen name="dashboad" component={Dashboard} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -64,5 +113,11 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
-
+  homePageHeader:{
+    backgroundColor:"#000f89",
+    height:"10vh",
+    },
+    HomeHeaderTitle:{
+      color:"#fff",
+    }
 });
--- pages/UsersPage/HomePage.js
+++ pages/UsersPage/HomePage.js
@@ -1,15 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import {Tabs} from 'expo-router'
+import { Tabs } from 'expo-router'
 
 
 const HomePage = () => {
   return (
-      <View >
-        <Tabs>
-          <Tabs.Screen name='Home Page'/>
-          <Tabs.Screen name='Home Page' />
-        </Tabs>
+      <View style={styles.container} >
+        <Text style={styles.title}>Home Page</Text>
       </View>
   );
 };
